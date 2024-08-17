6a76cbafbe4db34ef9976fe1974fe5b9818bbadc
JoaoLuizGL <joaoluiz.glira@gmail.com>
2024-08-17 03:46:48 -0300

    commit 1

---
+++ RetornoAlfabeto/Program.cs
@@ -0,0 +1,31 @@
+﻿public class RetornoAlfabeto
+{
+    public void Letras(int letras)
+    {
+        char letra = 'A';
+
+        for (int i = 1; i <= letras; i++)
+        {
+            Console.Write(i + "- ");
+            Console.WriteLine(letra);
+            letra++;
+
+        }
+
+    }
+    public static void Main()
+    {
+        int letras;
+        RetornoAlfabeto alfabeto = new RetornoAlfabeto();
+
+        do
+        {
+            Console.Clear();
+            Console.Write("Digite quantas letras do alfabeto serão retornadas: ");
+            letras = int.Parse(Console.ReadLine());
+        } while (letras < 1 || letras > 26);
+
+        alfabeto.Letras(letras);
+        Console.ReadKey();
+    }
+}
